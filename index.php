d1a660819ef9064fffb3807685840d1910d3a606
Brian Shaler <brian@shaler.name>
2010-11-15 14:57:14 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+<title>PHXdata.org - Data mining, processing, and visualization group in Phoenix, AZ</title>
+<link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8">
+</head>
+<body>
+
+<div id="all">
+    <div id="content">
+        <div id="header">
+            <div>
+                Civic Hacking 1st Tues @ Cronkite School of Journalism<br />
+                Data User Group 3rd Tues @ Gangplank
+            </div>
+        </div>
+
+        <p>PHXdata seeks to unite technologists in the Phoenix area who are engaged in data mining, 
+        parsing, visualization, etc. It also serves as a platform for journalists and government officials 
+        to connect with civic hackers who want to take public data and make it useful.</p>
+        
+        <p>Join the mailing list at: <a href="http://groups.google.com/group/phxdata">http://groups.google.com/group/phxdata</a></p>
+
+        <div class="box">
+            <div class="box-content">
+                <h1 class="box-header">
+                    Civic Hacking
+                </h1>
+                <p>Workgroup taking public data and 
+                making it available, useful, 
+                and meaningful.</p>
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td width="60px" valign="top">Who:</td>
+                        <td>Data Technologists<br />
+                        	Journalists<br />
+                        	Government Officials</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="height: 12px;"><!-- --></td>
+                    </tr>
+                    <tr>
+                        <td valign="top">When:</td>
+                        <td>1st Tues of every month</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="height: 12px;"><!-- --></td>
+                    </tr>
+                    <tr>
+                        <td valign="top">Where:</td>
+                        <td>Cronkite School of Journalism<br />
+                        	555 N Central Ave Room 355<br />
+                        	Phoenix, AZ 85004</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="box" style="margin-left: 40px;">
+            <div class="box-content">
+                <h1 class="box-header">
+                    Data User Group
+                </h1>
+                <p>Presentations and discussions sharing
+                knowledge about technologies and 
+                tools to manipulate and visualize data.</p>
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td width="60px" valign="top">Who:</td>
+                        <td>Statisticians<br />
+                        	Developers<br />
+                        	Designers</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="height: 12px;"><!-- --></td>
+                    </tr>
+                    <tr>
+                        <td valign="top">When:</td>
+                        <td>3rd Tues of every month</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="height: 12px;"><!-- --></td>
+                    </tr>
+                    <tr>
+                        <td valign="top">Where:</td>
+                        <td>Gangplank<br />
+                        	325 E Elliot Rd<br />
+                            Chandler, AZ 85225</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <p>For more information, contact 
+            <a href="http://twitter.com/brianshaler">Brian Shaler</a>, 
+            <a href="http://twitter.com/heisenthought">Marc Chung</a>, or 
+            <a href="http://twitter.com/markng">Mark Ng</a>.
+        </p>
+
+    </div>
+</div>
+
+</body>
+</html>
